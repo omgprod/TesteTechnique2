e5f4415e9b4da67551dfcb65c7721aa3011a0bed
Brian Haggerty <brian.haggery@pitech.eu>
2019-04-04 14:20:59 +0200

    Upload

--- client/build/vue-loader.conf.js
+++ client/build/vue-loader.conf.js
@@ -7,6 +7,9 @@ const sourceMapEnabled = isProduction
   : config.dev.cssSourceMap
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/client/'
+    : '/',
   loaders: utils.cssLoaders({
     sourceMap: sourceMapEnabled,
     extract: isProduction
